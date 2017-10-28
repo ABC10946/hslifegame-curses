5a0f71d733f9dfd07111c39154ea373885a26321
rect1114 <rectangle1114@gmail.com>
2017-10-28 11:26:09 +0900

    getch not worked, so copied in Main.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,33 @@
 import Lifegame
+import qualified UI.HSCurses.Curses as Curses
+import qualified UI.HSCurses.CursesHelper as CursesHelper
+import Control.Monad (forever)
+import System.Exit (exitWith,ExitCode (..))
 
 main :: IO ()
 main = do
-    let field = (foldl (fieldChange True) (initField (30,20))) [(5,5),(6,6),(6,7),(5,7),(4,7)]
-    loop field 100
+    CursesHelper.start
+    (height,width) <- Curses.scrSize
+    let field = Lifegame.initField (width-5,height-5)
+    cursesOutput field (length field)
+    forever $ do
+        c <- Curses.getCh -- getCh is not worked in 'stack test'
+        case CursesHelper.displayKey c of "q" -> quitTask
+                                          _   -> return ()
+
+quitTask :: IO ()
+quitTask = do
+    CursesHelper.end
+    exitWith ExitSuccess
+
+cursesOutput :: Field -> Int -> IO ()
+cursesOutput field lineCount = do
+    if lineCount /= 0 then do
+        Curses.wAddStr Curses.stdScr (lineToString (field !! ((length field) - lineCount)))
+        Curses.addLn
+        cursesOutput field (lineCount - 1)
+    else
+        return ()
 
 loop :: Field -> Int -> IO ()
 loop field times = do
