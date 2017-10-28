94d3f76160dd77e88aa8637814ecb498787f0080
rect1114 <rectangle1114@gmail.com>
2017-10-28 11:25:36 +0900

    test for getch

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
 module Main where
 
 import Lifegame
+import qualified UI.HSCurses.Curses as Curses
+import qualified UI.HSCurses.CursesHelper as CursesHelper
+import Control.Monad (forever)
+import System.Exit (exitWith,ExitCode (..))
 
 main :: IO ()
-main = return ()
+main = do
+    -- loop field 100
+    CursesHelper.start
+    (height,width) <- Curses.scrSize
+    let fieldInit = Lifegame.initField (width,height)
+    let field = (foldl (fieldChange True) fieldInit) [(5,5),(6,6),(6,7),(5,7),(4,7)]
+    loop field
+
+quitTask :: IO ()
+quitTask = do
+    CursesHelper.end
+    exitWith ExitSuccess
+
+cursesOutput :: Field -> Int -> IO ()
+cursesOutput field lineCount = do
+    if lineCount /= 0 then do
+        Curses.wAddStr Curses.stdScr (lineToString (field !! ((length field) - lineCount)))
+        Curses.addLn
+        cursesOutput field (lineCount - 1)
+    else
+        return ()
+
+loop :: Field -> IO ()
+loop field = do
+    let sentineledField = addSentineled $ addSentineled field 
+    cursesOutput sentineledField (length sentineledField)
+    Curses.refresh
+    CursesHelper.gotoTop
+    Curses.timeout 500
+    c <- CursesHelper.getKey (return ())
+    case CursesHelper.displayKey c of "q" -> quitTask
+                                      _   -> loop (step field)
